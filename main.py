9ffa75c6547f31336d3db15e2825c2d6d2d5776e
KingShark1 <manastiwari28@gmail.com>
2024-09-12 16:30:59 +0530

    add pool size and overflow in initializing engine

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ import json
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./swimming_competition.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
