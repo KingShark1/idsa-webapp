4dbb41345c5677838c21f04051d353fdd155d010
KingShark1 <manastiwari28@gmail.com>
2025-05-17 15:34:12 +0530

    updated for district 2025
    Signed-off-by: KingShark1 <manastiwari28@gmail.com>

--- load_events.py
+++ load_events.py
@@ -1,11 +1,16 @@
 import json
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, Event
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base, Event
+
 # Database setup
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:axUOJvfmpJyrVQPDjepPvplCSvuEhYFU@autorack.proxy.rlwy.net:35895/railway")
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:RxkaolbdRsElSWyRGELrCraSfzXvRdnE@yamanote.proxy.rlwy.net:14562/railway",
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -14,12 +19,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(engine)  # This line ensures all tables are created.
 
 import csv
-import json
+
 
 def convert_csv_to_json(csv_file, json_file):
     events = []
 
-    with open('static/config.json', 'r') as config_file:
+    with open("static/config.json", "r") as config_file:
         config = json.load(config_file)
 
     assert "age_group_map" in config
@@ -28,7 +33,7 @@ def convert_csv_to_json(csv_file, json_file):
 
     # Age group and gender mapping
 
-    with open(csv_file, newline='') as csvfile:
+    with open(csv_file, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             event = {
@@ -36,15 +41,16 @@ def convert_csv_to_json(csv_file, json_file):
                 "name": row["Event"],
                 "age_group": config["age_group_map"].get(row["Age Group"], -1),
                 "gender": config["gender_map"].get(row["Gender"], "unknown"),
-                "time_trial": config["time_trial_map"].get(row["Mode"], 1)
-
+                "time_trial": config["time_trial_map"].get(row["Mode"], 1),
             }
             events.append(event)
 
-    with open(json_file, 'w') as jsonfile:
+    with open(json_file, "w") as jsonfile:
         json.dump(events, jsonfile, indent=4)
 
+
 # Usage
+
 
 def reset_database():
     """
@@ -74,8 +80,16 @@ def load_events():
             events = json.load(file)
 
         # Check if the event already exists to prevent duplicates
-        new_events = [Event(id=e["id"], name=e["name"], age_group=e["age_group"], gender=e["gender"], time_trial=e["time_trial"])
-                      for e in events]
+        new_events = [
+            Event(
+                id=e["id"],
+                name=e["name"],
+                age_group=e["age_group"],
+                gender=e["gender"],
+                time_trial=e["time_trial"],
+            )
+            for e in events
+        ]
 
         db.add_all(new_events)
         db.commit()
@@ -84,7 +98,8 @@ def load_events():
     finally:
         db.close()
 
+
 if __name__ == "__main__":
     reset_database()
-    convert_csv_to_json("Schools_District_2024.csv", 'events.json')
+    convert_csv_to_json("district_event_chart_2025.csv", "events.json")
     load_events()
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from fastapi import FastAPI, Depends
+from dotenv import load_dotenv
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-
-from routes import router as routes_app
-from models import Event, Base
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
-load_dotenv()
-from pathlib import Path
-import os
-import json
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:dmuqxqhpwlVTDdRdIyEULeontdCSdFOu@autorack.proxy.rlwy.net:37151/railway")
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,
-                       pool_size=20, max_overflow=0)
+from models import Base
+from routes import router as routes_app
+
+load_dotenv()
+import os
+from pathlib import Path
+
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:RxkaolbdRsElSWyRGELrCraSfzXvRdnE@yamanote.proxy.rlwy.net:14562/railway",
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -43,4 +45,5 @@ app.mount(
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
