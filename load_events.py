1eb496d399d0b3f0231e8a6419e69c4d25ae9aad
KingShark1 <manastiwari28@gmail.com>
2025-11-06 13:53:10 +0530

    update home.html

--- load_events.py
+++ load_events.py
@@ -1,11 +1,18 @@
 import json
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, Event
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Base, Event
+
 # Database setup
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:ErmWsGnksaBwaCWStClxMdAhUTAFILmP@postgres.railway.internal:5432/railway")
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:PQLuvDObCtbwowFJYIDVbXtiCwTtGHDj@ballast.proxy.rlwy.net:52455/railway",
+)
+# FILE PATH
+FILE_NAME = "schools-2025-states-eventchart.csv"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -13,12 +20,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(engine)  # This line ensures all tables are created.
 
 import csv
-import json
+
 
 def convert_csv_to_json(csv_file, json_file):
     events = []
 
-    with open('static/config.json', 'r') as config_file:
+    with open("static/config.json", "r") as config_file:
         config = json.load(config_file)
 
     assert "age_group_map" in config
@@ -27,7 +34,7 @@ def convert_csv_to_json(csv_file, json_file):
 
     # Age group and gender mapping
 
-    with open(csv_file, newline='') as csvfile:
+    with open(csv_file, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             event = {
@@ -35,13 +42,13 @@ def convert_csv_to_json(csv_file, json_file):
                 "name": row["Event"],
                 "age_group": config["age_group_map"].get(row["Age Group"], -1),
                 "gender": config["gender_map"].get(row["Gender"], "unknown"),
-                "time_trial": config["time_trial_map"].get(row["Mode"], 1)
-
+                "time_trial": config["time_trial_map"].get(row["Mode"], 1),
             }
             events.append(event)
 
-    with open(json_file, 'w') as jsonfile:
+    with open(json_file, "w") as jsonfile:
         json.dump(events, jsonfile, indent=4)
+
 
 # Usage
 
@@ -55,8 +62,16 @@ def load_events():
             events = json.load(file)
 
         # Check if the event already exists to prevent duplicates
-        new_events = [Event(id=e["id"], name=e["name"], age_group=e["age_group"], gender=e["gender"], time_trial=e["time_trial"])
-                      for e in events]
+        new_events = [
+            Event(
+                id=e["id"],
+                name=e["name"],
+                age_group=e["age_group"],
+                gender=e["gender"],
+                time_trial=e["time_trial"],
+            )
+            for e in events
+        ]
 
         db.add_all(new_events)
         db.commit()
@@ -65,6 +80,7 @@ def load_events():
     finally:
         db.close()
 
+
 if __name__ == "__main__":
-    convert_csv_to_json('CBSE West Zone 2024 - EventChart.csv', 'events.json')
+    convert_csv_to_json(FILE_NAME, "events.json")
     load_events()
